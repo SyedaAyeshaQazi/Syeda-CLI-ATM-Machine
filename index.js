709587a9aeaaf0f7f4cc46064f8e071ec05eaefd
SyedaAyeshaQazi <160398956+SyedaAyeshaQazi@users.noreply.github.com>
2024-03-30 02:33:50 +0500

    my ATM commit

---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybalance = 10000;
+let mypin = 26328;
+const pinAnswere = await inquirer.prompt({
+    name: "YourEnteredPin",
+    message: "enter your pin",
+    type: "number"
+});
+// console.log(pinAnswere.YourEnteredPin);
+if (pinAnswere.YourEnteredPin === mypin) {
+    console.log("correct pin!!!");
+    const operationAnsweres = await inquirer.prompt({
+        name: "operation",
+        message: "Please Select Option",
+        type: "list",
+        choices: ["Withdraw", "Check Balance"]
+    });
+    if (operationAnsweres.operation === "Withdraw") {
+        const amountAnswere = await inquirer.prompt([
+            {
+                name: "Amount",
+                message: "Enter Your Amount",
+                type: "number"
+            }
+        ]);
+        if (mybalance >= amountAnswere.Amount) {
+            const remaining = mybalance - amountAnswere.Amount;
+            console.log(`Your Remaining Balance is ${mybalance}`);
+        }
+        else {
+            console.log("Insufficient Balance");
+        }
+    }
+    else if (operationAnsweres.operation === "Check Balance") {
+        console.log(`Your Current Balance is ${mybalance}`);
+    }
+}
+else {
+    console.log("incorrect pin");
+}
